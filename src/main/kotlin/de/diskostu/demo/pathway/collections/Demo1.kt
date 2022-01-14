45885edc8e625545c63b7dd8420bbcf3a1468871
diskostu <denny.kluge@gmail.com>
2022-01-14 09:33:52 +0100

    added some collection examples

---
+++ src/main/kotlin/de/diskostu/demo/pathway/collections/Demo1.kt
@@ -0,0 +1,20 @@
+package de.diskostu.demo.pathway.collections
+
+fun main() {
+    val words = listOf("about",
+        "acute",
+        "awesome",
+        "balloon",
+        "best",
+        "brief",
+        "class",
+        "coffee",
+        "creative")
+    val filteredWords = words
+        .filter { it.startsWith("b", ignoreCase = true) }
+        .shuffled()
+        .take(2)
+        .sorted()
+
+    println(filteredWords)
+}
