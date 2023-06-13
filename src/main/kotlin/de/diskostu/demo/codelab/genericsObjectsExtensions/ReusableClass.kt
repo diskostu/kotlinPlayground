0451515ce05a989f1ec53ffa6f3746d73621ce12
Denny Kluge <denny.kluge@gmail.com>
2023-06-13 17:05:02 +0200

    used an enum class

--- src/main/kotlin/de/diskostu/demo/codelab/genericsObjectsExtensions/ReusableClass.kt
+++ src/main/kotlin/de/diskostu/demo/codelab/genericsObjectsExtensions/ReusableClass.kt
@@ -2,9 +2,9 @@ package de.diskostu.demo.codelab.genericsObjectsExtensions
 
 fun main() {
     val questionText = "what is the answer?"
-    val genericQuestion1 = GenericQuestion(questionText, 42, "easy")
-    val genericQuestion2 = GenericQuestion(questionText, true, "easy")
-    val genericQuestion3 = GenericQuestion(questionText, "universe", "easy")
+    val genericQuestion1 = GenericQuestion(questionText, 42, Difficulty.EASY)
+    val genericQuestion2 = GenericQuestion(questionText, true, Difficulty.MEDIUM)
+    val genericQuestion3 = GenericQuestion(questionText, "universe", Difficulty.HARD)
 
     val answer1 = genericQuestion1.answer
     val answer2 = genericQuestion2.answer
@@ -13,10 +13,16 @@ fun main() {
     println("answer1 = $answer1")
     println("answer2 = $answer2")
     println("answer3 = $answer3")
+
+    println("difficulty1 = ${genericQuestion1.difficulty}")
 }
 
 
 /**
  * Instead of creating 3 different classes because the data type of "answer" can be different, we use Generics.
  */
-class GenericQuestion<T>(val questionText: String, val answer: T, val difficulty: String)
+class GenericQuestion<T>(val questionText: String, val answer: T, val difficulty: Difficulty)
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
